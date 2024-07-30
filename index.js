1e85dbb51a1faa52cad3ba90f2e6411927eaaef9
david30-maker <davidokpah396@gmail.com>
2024-07-30 12:43:16 +0100

    create product

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ const products = [
     {id: 3, name: 'Hub', price: 30},
     {id: 4, name: 'Access Point', price: 80}
 ];
+
+app.get('/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+    const product = products.find(p => p.id === parseInt(req.params.id));
+    if (!product) {
+        return res.status(404).send('Product not found');
+    }
+    res.json(product);
+});
+
